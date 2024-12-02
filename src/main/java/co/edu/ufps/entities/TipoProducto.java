d52c3f62fd6226fdc3321c5fe748538e59304e36
LeydiD <leydialejaduran@gmail.com>
2024-12-02 10:38:48 -0500

    Entities producto y tipo producto

---
+++ src/main/java/co/edu/ufps/entities/TipoProducto.java
@@ -0,0 +1,35 @@
+package co.edu.ufps.entities;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="tipo_snack")
+@Data
+public class TipoProducto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@OneToMany(mappedBy = "tipoProducto", cascade= CascadeType.ALL)
+	@JsonIgnore
+	private List<Producto> productos;
+	
+	
+}
